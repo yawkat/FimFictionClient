7ad17079108a1353c7f39e9e0327ab0544af4c45
yawkat <me@yawk.at>
2012-12-11 21:04:29 +0100

    Added an SQLite-Based backup utility

--- src/at/yawk/fimfiction/Stories.java
+++ src/at/yawk/fimfiction/Stories.java
@@ -41,41 +41,47 @@ public final class Stories {
 		downloadStory(s, output, downloadType, ffc, null);
 	}
 	
-	public static void updateStory(final Story s, IFimFictionConnection ffc) throws MalformedURLException, IOException, JSONException {
+	public static boolean updateStory(final Story s, IFimFictionConnection ffc) throws MalformedURLException, IOException, JSONException {
 		final JSONTokener jst = new JSONTokener(Util.readFully(ffc.getConnection(new URL(Util.FIMFICTION + "api/story.php?story=" + s.getId())).getInputStream()));
-		final JSONObject jso = ((JSONObject)jst.nextValue()).getJSONObject("story");
-		s.setTitle(jso.getString("title"));
-		s.setShortDescription(jso.getString("short_description"));
-		s.setDescription(jso.getString("description"));
-		s.setModifyTime(new Date(jso.getLong("date_modified") * 1000L));
-		if(jso.has("image"))
-			s.setImageLocation(jso.getString("image"));
-		if(jso.has("full_image"))
-			s.setFullImageLocation(jso.getString("full_image"));
-		s.setViews(jso.getInt("views"));
-		s.setTotalViews(jso.getInt("total_views"));
-		s.setWords(jso.getInt("words"));
-		s.setComments(jso.getInt("comments"));
-		s.setStatus(EnumStoryStatus.parse(jso.getString("status")));
-		s.setContentRating(EnumStoryContentRating.parse(jso.getString("content_rating_text")));
-		s.setLikes(jso.getInt("likes"));
-		s.setDislikes(jso.getInt("dislikes"));
-		s.setAuthor(new Author(Integer.parseInt(jso.getJSONObject("author").getString("id"))));
-		s.getAuthor().setName(jso.getJSONObject("author").getString("name"));
-		if(jso.has("chapters")) {
-			final JSONArray chapters = jso.getJSONArray("chapters");
-			s.setChapters(new Chapter[Math.min(jso.getInt("chapter_count"), chapters.length())]);
-			for(int i = 0; i < s.getChapters().length; i++) {
-				final JSONObject cj = chapters.getJSONObject(i);
-				final Chapter c = new Chapter(cj.getInt("id"), s, i);
-				c.setTitle(cj.getString("title"));
-				c.setWords(cj.getInt("words"));
-				c.setViews(cj.getInt("views"));
-				c.setModifyTime(new Date(cj.getLong("date_modified") * 1000L));
-				s.getChapters()[i] = c;
+		final JSONObject nxt = (JSONObject)jst.nextValue();
+		if(nxt.has("story")) {
+			final JSONObject jso = ((JSONObject)nxt).getJSONObject("story");
+			s.setTitle(jso.getString("title"));
+			s.setShortDescription(jso.getString("short_description"));
+			s.setDescription(jso.getString("description"));
+			s.setModifyTime(new Date(jso.getLong("date_modified") * 1000L));
+			if(jso.has("image"))
+				s.setImageLocation(jso.getString("image"));
+			if(jso.has("full_image"))
+				s.setFullImageLocation(jso.getString("full_image"));
+			s.setViews(jso.getInt("views"));
+			s.setTotalViews(jso.getInt("total_views"));
+			s.setWords(jso.getInt("words"));
+			s.setComments(jso.getInt("comments"));
+			s.setStatus(EnumStoryStatus.parse(jso.getString("status")));
+			s.setContentRating(EnumStoryContentRating.parse(jso.getString("content_rating_text")));
+			s.setLikes(jso.getInt("likes"));
+			s.setDislikes(jso.getInt("dislikes"));
+			s.setAuthor(new Author(Integer.parseInt(jso.getJSONObject("author").getString("id"))));
+			s.getAuthor().setName(jso.getJSONObject("author").getString("name"));
+			if(jso.has("chapters")) {
+				final JSONArray chapters = jso.getJSONArray("chapters");
+				s.setChapters(new Chapter[Math.min(jso.getInt("chapter_count"), chapters.length())]);
+				for(int i = 0; i < s.getChapters().length; i++) {
+					final JSONObject cj = chapters.getJSONObject(i);
+					final Chapter c = new Chapter(cj.getInt("id"), s, i);
+					c.setTitle(cj.getString("title"));
+					c.setWords(cj.getInt("words"));
+					c.setViews(cj.getInt("views"));
+					c.setModifyTime(new Date(cj.getLong("date_modified") * 1000L));
+					s.getChapters()[i] = c;
+				}
+			} else {
+				s.setChapters(new Chapter[0]);
 			}
+			return true;
 		} else {
-			s.setChapters(new Chapter[0]);
+			return false;
 		}
 	}
 }
---
+++ src/at/yawk/fimfiction/examples/sqlitedl/SQLiteDownloader.java
@@ -0,0 +1,222 @@
+package at.yawk.fimfiction.examples.sqlitedl;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.json.JSONException;
+
+import at.yawk.fimfiction.Chapter;
+import at.yawk.fimfiction.EnumDownloadType;
+import at.yawk.fimfiction.EnumSearchOrder;
+import at.yawk.fimfiction.FimFictionConnectionStandard;
+import at.yawk.fimfiction.IFimFictionConnection;
+import at.yawk.fimfiction.SearchRequestBuilder;
+import at.yawk.fimfiction.Searches;
+import at.yawk.fimfiction.Stories;
+import at.yawk.fimfiction.Story;
+import at.yawk.fimfiction.Util;
+
+public class SQLiteDownloader implements Runnable {
+	public static void main(String[] args) {
+		new SQLiteDownloader().run();
+	}
+	
+	private Connection					databaseConnection;
+	private Lock						connectionLock			= new ReentrantLock();
+	
+	private final File					storyDownloadDirectory	= new File("data");
+	private final IFimFictionConnection	connection				= new FimFictionConnectionStandard();
+	
+	private SQLiteDownloader() {
+		storyDownloadDirectory.mkdirs();
+	}
+	
+	private void createTables() throws SQLException {
+		databaseConnection.createStatement().execute("CREATE TABLE IF NOT EXISTS authors (id INTEGER PRIMARY KEY, authorid int(16), name varchar(100))");
+		databaseConnection.createStatement().execute("CREATE TABLE IF NOT EXISTS chapters (id INTEGER PRIMARY KEY AUTOINCREMENT, updateid int(10), chapterid int(10), storyindex int(4), title varchar(100), words int(10), views int(10), modifytime date);");
+		databaseConnection.createStatement().execute("CREATE TABLE IF NOT EXISTS stories (id INTEGER PRIMARY KEY AUTOINCREMENT, storyid int(8), description varchar(10000), shortdescription varchar(10000), title varchar(100), modifytime date, imagelocation varchar(300), fullimagelocation varchar(300), views int(6), totalviews int(6), comments int(6), authorid int(16), status varchar(30), contentrating varchar(30), likes int(6), dislikes int(6), words int(6), filename varchar(32));");
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Starting");
+		connect();
+		try {
+			createTables();
+		} catch(SQLException e1) {
+			e1.printStackTrace();
+		}
+		
+		final int maxId = Searches.parseFullSearchPartially(Util.FIMFICTION + "index.php?" + new SearchRequestBuilder().setSearchOrder(EnumSearchOrder.FIRST_POSTED_DATE), connection, 0).next().getId() + 1000;
+		
+		final Executor ex = Executors.newFixedThreadPool(40);
+		final AtomicInteger ai = new AtomicInteger();
+		for(int i = 0; i < maxId; i++) {
+			final int index = i;
+			ai.incrementAndGet();
+			ex.execute(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						updateStory(new Story(index));
+					} catch(SQLException | IOException | JSONException e) {
+						e.printStackTrace();
+					} finally {
+						ai.decrementAndGet();
+					}
+				}
+			});
+		}
+		
+		while(ai.get() > 0) {
+			try {
+				TimeUnit.SECONDS.sleep(2);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		disconnect();
+		System.out.println("Stopping");
+		System.exit(0);
+	}
+	
+	private void connect() {
+		try {
+			System.out.println("Loading SQL driver");
+			Class.forName("org.sqlite.JDBC");
+			System.out.println("Connecting");
+			databaseConnection = DriverManager.getConnection("jdbc:sqlite:fimfiction.db");
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void disconnect() {
+		try {
+			System.out.println("Disconnecting");
+			databaseConnection.close();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private File getNewRandomEpubFile() {
+		File f;
+		do {
+			f = new File(storyDownloadDirectory, RandomStringUtils.randomAlphanumeric(16) + "." + EnumDownloadType.EPUB.getFileType());
+		} while(f.exists());
+		return f;
+	}
+	
+	private void updateStory(Story s) throws IOException, JSONException, SQLException {
+		if(Stories.updateStory(s, connection)) {
+			final ResultSet result = databaseConnection.createStatement().executeQuery("SELECT * FROM stories WHERE storyid = '" + s.getId() + "'");
+			final int requestLength;
+			{
+				final ResultSet rezLen = databaseConnection.createStatement().executeQuery("SELECT count(*) FROM stories WHERE storyid = '" + s.getId() + "'");
+				requestLength = rezLen.getInt(1);
+			}
+			
+			final boolean existsAlready = requestLength > 0;
+			for(int i = 1; i < requestLength; i++)
+				result.next();
+			final boolean outdatedEntry = existsAlready && result.getDate("modifytime").getTime() != s.getModifyTime().getTime();
+			if(outdatedEntry || !existsAlready) {
+				System.out.println("Updating story " + s.getId() + " (" + s.getTitle().replace("\n", "") + ")");
+				final File epub = getNewRandomEpubFile();
+				final OutputStream os = new FileOutputStream(epub);
+				Stories.downloadStory(s, os, EnumDownloadType.EPUB, connection);
+				os.close();
+				
+				final PreparedStatement prepared = databaseConnection.prepareStatement("INSERT INTO stories (storyid, description, shortdescription, title, modifytime, imagelocation, fullimagelocation, views, totalviews, comments, authorid, status, contentrating, likes, dislikes, words, filename) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+				int index = 1;
+				prepared.setInt(index++, s.getId());
+				prepared.setString(index++, s.getDescription());
+				prepared.setString(index++, s.getShortDescription());
+				prepared.setString(index++, s.getTitle());
+				prepared.setDate(index++, new Date(s.getModifyTime().getTime()));
+				prepared.setString(index++, s.getImageLocation());
+				prepared.setString(index++, s.getFullImageLocation());
+				prepared.setInt(index++, s.getViews());
+				prepared.setInt(index++, s.getTotalViews());
+				prepared.setInt(index++, s.getComments());
+				prepared.setInt(index++, s.getAuthor().getId());
+				prepared.setString(index++, s.getStatus().toString());
+				prepared.setString(index++, s.getContentRating().toString());
+				prepared.setInt(index++, s.getLikes());
+				prepared.setInt(index++, s.getDislikes());
+				prepared.setInt(index++, s.getWords());
+				prepared.setString(index++, epub.getName());
+				
+				final ResultSet generatedKeys;
+				connectionLock.lock();
+				try {
+					prepared.execute();
+					generatedKeys = prepared.getGeneratedKeys();
+				} finally {
+					connectionLock.unlock();
+				}
+				final int totalId = generatedKeys.getInt(1);
+				for(Chapter c : s.getChapters()) {
+					final PreparedStatement cprepared = databaseConnection.prepareStatement("INSERT INTO chapters (updateid, chapterid, storyindex, title, words, views, modifytime) VALUES (?, ?, ?, ?, ?, ?, ?)");
+					index = 1;
+					cprepared.setInt(index++, totalId);
+					cprepared.setInt(index++, c.getId());
+					cprepared.setInt(index++, c.getStoryIndex());
+					cprepared.setString(index++, c.getTitle());
+					cprepared.setInt(index++, c.getWords());
+					cprepared.setInt(index++, c.getViews());
+					cprepared.setDate(index++, new Date(c.getModifyTime().getTime()));
+					cprepared.execute();
+				}
+				
+				final int authorLength;
+				{
+					final ResultSet rezLen = databaseConnection.createStatement().executeQuery("SELECT count(*) FROM authors WHERE authorid = '" + s.getAuthor().getId() + "'");
+					authorLength = rezLen.getInt(1);
+				}
+				final ResultSet authorGetRequest = databaseConnection.createStatement().executeQuery("SELECT * FROM authors WHERE authorid = '" + s.getAuthor().getId() + "'");
+				final boolean insertAuthor;
+				if(authorLength > 0) {
+					for(int i = 1; i < authorLength; i++)
+						authorGetRequest.next();
+					insertAuthor = !authorGetRequest.getString("name").equals(s.getAuthor().getName());
+				} else {
+					insertAuthor = true;
+				}
+				if(insertAuthor) {
+					final PreparedStatement aprepared = databaseConnection.prepareStatement("INSERT INTO authors (authorid, name) VALUES (?, ?)");
+					aprepared.setInt(1, s.getAuthor().getId());
+					aprepared.setString(2, s.getAuthor().getName());
+					aprepared.execute();
+				}
+			}
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	private void truncateDB() throws SQLException {
+		databaseConnection.createStatement().execute("DELETE FROM stories");
+		databaseConnection.createStatement().execute("DELETE FROM chapters");
+		databaseConnection.createStatement().execute("DELETE FROM authors");
+	}
+}
